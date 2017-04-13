92bfe81af5da91629d843622532dbce28bb5019a
Joe Gibbs Politz <joe.politz@gmail.com>
2017-04-13 10:43:47 -0700

    .

---
+++ stack-map/examples/gather-trace.js
@@ -0,0 +1,17 @@
+// theMap is defined in the .map file
+const consumer = new sourceMap.SourceMapConsumer(theMap);
+
+try {
+  start(); // start is the generated entry point
+}
+catch(e) {
+  console.log("The error is: ", e);
+  const parsedStack = ErrorStackParser.parse(e);
+  console.log("Parsed stack: ", parsedStack);
+  console.log("All generated positions on stack: ");
+  parsedStack.forEach(function(elt) {
+    console.log(consumer.originalPositionFor({ source: "test.arr", line: elt.lineNumber, column: elt.columnNumber }));
+  });
+}
+
+
